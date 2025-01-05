aef2b58e8b59f496175dbd9f6f77ef0fdbfc0f3f
bluecrest22 <bluecrest22@gmail.com>
2025-01-05 14:02:23 -0500

    Added image upload

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,16 +1,30 @@
+"use client";
+
 import { SignedOut, SignInButton, UserButton, SignedIn } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
+
+
 
 export function TopNav() {
+  const router = useRouter();
+
   return (
     <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold">
       <div>Gallery</div>
 
-      <div>
+      <div className="flex flex-row">
         <SignedOut>
             <SignInButton />
         </SignedOut>
         <SignedIn>
-            <UserButton />
+          <UserButton />
+          <UploadButton 
+          endpoint="imageUploader"
+          onClientUploadComplete={() => {
+            router.refresh();
+          }}
+          />
         </SignedIn>
       </div>
     </nav>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import "~/styles/globals.css";
+import "@uploadthing/react/styles.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { TopNav } from "./_components/topnav";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "~/app/api/uploadthing/core";
+
 
 export const metadata: Metadata = {
   title: "T3 Gallery",
@@ -17,6 +22,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
+        <NextSSRPlugin
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
         <body className={`font-sans flex-col gap-4`}>
           <TopNav />
           {children}
